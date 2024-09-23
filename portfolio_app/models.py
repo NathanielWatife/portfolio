a1d568fe457e0c5cf97f982f929cee4d9cca3130
NathanielWatife <awosanminathniel0@gmail.com>
2024-09-23 20:28:49 +0100

    Worked on portfolio settings and app

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Skill(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+    
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to="projects/")
+    project_url = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    
--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def home(request):
+    skills = Skill.objects.all() 
+    projects = Project.objects.all()
+    return render(request, "portfolio_app/home.html", {
+        "skills": skills,
+        "projects": projects
+    })
